5f82189f17e24612601383eea5f9415ed86e05db
microassest <142403686+microassest@users.noreply.github.com>
2024-07-01 09:17:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,19 +61,7 @@ let semkda={
     s306:[ "130CS22306", "SHIVAKUMAR",0,37]
     
     };
-    
-    
-let dcetDATA={
-    //reg last  reg       name     D REG   RANK   D SCIRE DIP C
-    s005:["130CS21005","AMITH G S","J2495","1919","63.000","81"],
-    s062:["130CS21062","VIGNESH D"," J3331","1880","62.000","82.3"],
-    s012:["130CS21012","CHANDRASHEKHAR S","J1255","509","80","82"],
-    
-    //s000:[" "," "," "," "," "," "],
-    
-    
-    };
-    
+     
     
 
 let sem6da={
